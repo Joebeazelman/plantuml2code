d176d1fa0242cdd6c6f9e15ad90f7e675a72b83b
joebeazelman <joebeazelman@gmail.com>
2026-09-20 09:20:11 -0400

    State generator: AUnit test suite for semantic invariants

--- plantuml2code/tests/all_tests.adb
+++ plantuml2code/tests/all_tests.adb
@@ -3,6 +3,7 @@ with Test_Ansi;
 with Test_CLI;
 with Test_Formats;
 with Test_Generator_Class;
+with Test_Generator_States;
 
 function All_Tests return Access_Test_Suite is
    Result : constant Access_Test_Suite := new AUnit.Test_Suites.Test_Suite;
@@ -11,5 +12,6 @@ begin
    Result.Add_Test (new Test_CLI.Case_Type);
    Result.Add_Test (new Test_Formats.Case_Type);
    Result.Add_Test (new Test_Generator_Class.Case_Type);
+   Result.Add_Test (new Test_Generator_States.Case_Type);
    return Result;
 end All_Tests;
